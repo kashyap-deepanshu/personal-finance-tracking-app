6e46c8d9a67c6594a4ad77429cbf7c0c6b7d4a38
Deepanshu <kashyap.deepanshu.dk@gmail.com>
2026-02-18 11:07:30 +0530

    fix port issue

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -100,9 +100,9 @@ const Login = () => {
 
         {/* Divider */}
         <div className="flex items-center my-4">
-          <div className="flex-grow h-px bg-gray-200"></div>
+          <div className="grow h-px bg-gray-200"></div>
           <span className="px-3 text-gray-400 text-sm">OR</span>
-          <div className="flex-grow h-px bg-gray-200"></div>
+          <div className="grow h-px bg-gray-200"></div>
         </div>
 
         {/* Register Link */}
--- server/server.js
+++ server/server.js
@@ -9,7 +9,10 @@ const mongoose = require("mongoose");
 
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 const { protect } = require("./middleware/authMiddleware");
 
@@ -32,6 +35,3 @@ app.use("/api/auth", require("./routes/authRoutes"));
 
 app.use("/upload", uploadRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
